4f7a43de559e21edf4dc413bf2fda6df41af49c4
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2025-03-02 23:09:51 +0530

    Create gfk.java

---
+++ gfk.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Item{
+    int weight,profit;
+    double ratio;
+
+    Item(int weight,int profit)
+    {
+        this.weight=weight;
+        this.profit=profit;
+        this.ratio=(double) profit/weight;
+    }
+    
+
+}
+
+public class gfk{
+    public static double getProfit (Item[] items,int m)
+    {
+         Arrays.sort(items ,(a,b) -> Double.compare(b.ratio,a.ratio));
+         double maxprofit=0.0;
+         for(Item item:items)
+        {
+          if(item.weight<=m)
+          {
+            maxprofit+=item.profit;
+            System.out.println("Item: (Weight: " + item.weight + ", profit: " + item.profit + ") - Taken Fully");     
+             m -= item.weight;
+
+          }
+          else{
+            double fraction= (double) m/item.weight;
+            maxprofit +=item.profit;
+            System.out.println("Item: (Weight: " + item.weight*fraction + ",profit: " + item.profit*fraction + ") - Taken " + fraction * 100 + "%");
+            break;
+          }
+          
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args)
+{
+    Item[] items={new Item(10,60),new Item(20,100), new Item(30,120)};
+    int m=50;
+    double maxprofit= getProfit(items, m);
+    System.out.println("maxprofit"+"  "+ maxprofit);
+}
+}
+
+
+
+
